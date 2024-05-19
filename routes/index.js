4d134e482d9d37c6bf6e4b1e8e38efa3a4e35b9a
waleed <waleedmail@gmail.com>
2024-05-19 23:43:19 +0300

    error handling in GET middleware

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,29 @@ const router = require('express').Router();
 
 const urlSchema = require('../models/urlSchema.js'); // get the schema of url
 
+/*
+router.get('/favicon.ico', async function (req, res,next) {
+    const urls = await urlSchema.find({});
+    res.locals.allUrl = urls;
+    res.render('index', { title: 'Express' });
+});
+*/
+
 // get website using its alias Url
 router.get('/:aliasUrl', async (req, res) => {
-
     const urlCollection = await urlSchema.findOne({shortUrl :  req.params.aliasUrl});
-
-    //console.log(urlCollection);
-
-   res.redirect(urlCollection.originalUrl);
-
-  });
+    if(urlCollection){
+        console.log(urlCollection);
+        const link = urlCollection.originalUrl;
+        res.redirect(link);
+    }
+    else{
+        res.status(404).send('Not Found');
+    }
+});
 
 /* GET home page. */
-router.get('/', async function (req, res) {
+router.get('/', async function (req, res,next) {
     const urls = await urlSchema.find({});
     res.locals.allUrl = urls;
     res.render('index', { title: 'Express' });
