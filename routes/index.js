740724cc4bc93905f159ecce95538b7a030b1cb6
waleed <waleedmail@gmail.com>
2024-05-19 12:28:39 +0300

    GET home page updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const urlSchema = require('../models/urlSchema.js'); // get the schema of url
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+    const urls = await urlSchema.find({});
+    res.locals.allUrl = urls;
     res.render('index', { title: 'Express' });
 });
 
